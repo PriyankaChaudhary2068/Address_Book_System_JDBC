610feefb5d3203868414f3d981cef48917c7d411
chutki12 <priyankachaudhary2068>
2022-08-21 18:39:56 +0530

    ADDED : Connected Database To Java Program And Retrieved All The Data From Database Table

--- src/main/java/com/bridgelabz/addressBookSystemJDBC/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressBookSystemJDBC/AddressBookService.java
@@ -1,11 +1,37 @@
 package com.bridgelabz.addressBookSystemJDBC;
 
+import java.sql.*;
+
 public class AddressBookService {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Welcome To Address Book Service");
-
+	
+	    public static void main(String[] args) throws SQLException {
+	        System.out.println("Status of connecting database to java code: ");
+	        String jdbcURL = "jdbc:mysql://localhost:3306/address_book_service";
+	        String userName = "root";
+	        String password = "vchutki2068vpc";
+	        Connection connection = null;
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	            System.out.println("Driver loaded!");
+	            connection = DriverManager.getConnection(jdbcURL, userName, password);
+	            System.out.println("Connection done!!");
+	            Statement statement = connection.createStatement();
+	            ResultSet resultSet = statement.executeQuery("SELECT * FROM address_book");
+	            while (resultSet.next()) {
+	                System.out.println(resultSet.getString("firstName") + " | " + resultSet.getString("lastName") + " | " + resultSet.getString("address") + " | " + resultSet.getString("city") + " | " + resultSet.getString("state") + " | " + resultSet.getInt("zip") + " | " + resultSet.getInt("phone_Number") + " | " + resultSet.getString("email_id") + " | " + resultSet.getString("bookName") + " | " + resultSet.getString("contactType"));
+	            }
+	        } 
+	        catch (Exception e)
+	        {
+	            e.printStackTrace();
+	        }
+	        finally
+	        {
+	            connection.close();
+	        }
+	    }
 	}
+	
 
-}
+
